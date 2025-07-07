b114337e89119647f8c931968c8fa13535b752db
Eric Sorum <ejsorum@gmail.com>
2025-07-07 17:14:45 -0500

    Listed skills in Home.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,10 +12,13 @@ function Home() {
         <p className={style.subHeader}>
           Full Stack Web Developer with 2 years of experience.
         </p>
-        <p>Proficient in:
-          <span className={style.blueText}> JavaScript/TypeScript, PHP, HTML, CSS, React.js, </span>
-          and other web technologies.
-        </p>
+        <ul>
+          <li>Languages: JavaScript/TypeScript, PHP, SQL, HTML, CSS</li>
+          <li>Libraries & Tools: React.js, Tailwind CSS, Bootstrap, Drupal 8–10, Laravel, Node.js, Webpack</li>
+          <li>Dev Tools: Git, GitHub, Valet, Homebrew, Composer, npm/pnpm, </li>
+          <li>Practices: RESTful APIs, Agile development, Accessibility (WCAG 2.2), Cross-functional collaboration</li>
+          <li>Other: Cursor AI Agent, LLM-assisted development, Google Tag Manager, Google Analytics 4</li>
+        </ul>
         <div className={style.resumeButtons}>
           <a href={resume}>
             <div className={style.resumeButton}>View resume</div>
