35a0572c363b3eca1608c449503f11e014ba3f2d
Eric Sorum <ejsorum@gmail.com>
2025-07-08 12:13:50 -0500

    Removed some unused styles and elements in About.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import style from '../style/About.module.css';
 function About() {
   return (
     <div className={style.aboutDiv}>
-      <div className={style.aboutBlock}>
+      {/* <div className={style.aboutBlock}> */}
         <div className={style.headerDiv}>
           <h2 id="about" className={style.aboutHeader}>
             About Me
@@ -15,18 +15,19 @@ function About() {
         <div className={style.backgroundBox} />
         <div className={style.imgAndText}>
           <div>
-            <div className={style.headshotBox} />
-            <img src={headshot} alt="Eric Sorum" className={style.headshot} />
+            {/* <div className={style.headshotBox} /> */}
           </div>
           <div className={style.textBlock}>
+            <img src={headshot} alt="Eric Sorum" className={style.headshot} align="left"/>
+
             <p>
-            My life has been defined by self-starting and curiosity. It is no wonder that teaching myself to code and working as a developer has been on of the most fulfilling periods in my life.
+            My life has been defined by self-starting and curiosity. It is no wonder that teaching myself to code and working as a developer has been one of the most fulfilling parts of my life.
             </p>
             <p>
             I have been crazy about computers ever since I first played around on the DOS 
             prompt of my dad’s i386.  I went to computer camp in middle school to learn 
             C++ and dreamt of being a real programmer.  The past 17 years have seen me 
-            pursue many passions, representing a diverse set of experiences requiring 
+            pursue many other passions, representing a diverse set of experiences requiring 
             excellent communication and creativity:
             </p>
             <ul>
@@ -45,11 +46,11 @@ function About() {
             As a freelance Full Stack Developer, I am available for tasks large and small, whether it's styling your WordPress site or contributing to enterprise software.  My demonstrated drive to quickly pick up new technologies and thrive in a fast-paced environment make me a valuable addition to any project.  With over two years of experience as a developer, I built scores of web pages on the frontend while untangling the toughest backend bugs.  In all, I resolved over 1,000 Jira tickets, including large projects and urgent issues amid frequently changing priorities.
             <p>
             <p>I love the fact that I learn something new every day as a developer.  I pride myself on strong web fundamentals in HTML, CSS, and JavaScript, while exploring the latest technologies such as Next.js, Cursor AI, and Rust.</p>
-            <p>Outside of software, I enjoy performing in local theaters and singing J.S. Bach.  Oh, and if you get smashed by someone on the StarCraft 2 ladder, that may have been me.</p>
+            <p>Outside of software, I enjoy performing in local theaters and singing J.S. Bach.  Oh, and if you get crushed by someone on the StarCraft 2 ladder, that may have been me.</p>
             </p>
           </div>
         </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 }
