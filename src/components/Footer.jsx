6fb91ceace47c9d35898a5531bdcc3d6ac220f46
Eric Sorum <ejsorum@gmail.com>
2025-07-08 08:18:26 -0500

    Fixed footer alignment.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,12 @@ import github from './images/github.svg'
 
 function Footer() {
   return (
-    <div><a href="https://github.com/EJSorum/eric-sorum" 
-    className={style.footerDiv}><div>Designed and built by Eric Sorum </div>
-    <img src={github} alt="Github icon" className={style.linkIcon}/></a></div>
+    <div className={style.footerDiv}>
+      <div>
+        <a href="https://github.com/EJSorum/eric-sorum">Designed and built by Eric Sorum</a>
+      </div>
+      <a href="https://github.com/EJSorum/eric-sorum"><img src={github} alt="Github icon" className={style.linkIcon}/></a>
+    </div>
   )
 }
 
