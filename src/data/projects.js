7025983062fd37bd195d2b68e65aadea4fa9ff23
Eric Sorum <ejsorum@gmail.com>
2025-07-08 10:47:56 -0500

    Added descriptions to all projects.

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,32 +5,31 @@ export const projects = [
     name: "Keg Curator",
     github: "https://github.com/EricSorum/keg-curator",
     link: "https://keg-curator.vercel.app/",
-    description: "hihihi"
+    description: "Some restaurans and bars struggle when it comes to choosing the best available beer to serve. Many restauranteurs specialize in wine or cocktails, but not beer, and don't have the time to carefully curate a beer selection.\nNevertheless, good beer is a key area of profit, and an expectation for many customers. Even a small beer selection of just the right beer can complement any cuisine and keep customers coming back.\nThis app takes my 8 years of experience in the beer industry to create suggested menus based on a restaurant's needs. I provide suggestions from the best craft beeer available in Minnesota, as well as essential macro and import beers. The app makes selections based on a form input and creates a menu from algorithms.\nThe tech stack includes TypeScript, React.js, Shadcn UI components, and Tailwind CSS. Hosted on Vercel."
   },
   {
     name: "Videographer's Profile Site",
     github: "https://github.com/EricSorum/profile-site",
     link: "https://route-profile-site-eedf22cf5fdf.herokuapp.com/",
-    description: "hihihi"
+    description: "This site for a videography company demonstrates use of React, TypeScript, Tailwind, and React Router."
   },
   {
     name: "Korprulu Finance",
     github: "https://github.com/EricSorum/korprulu-finance",
     link: "https://ericsorum.github.io/korprulu-finance/",
-    description: "hihihi"
+    description: "Web fundamentals are back.  HTML and CSS become more powerful each year, seeing less need for JavaScript and style frameworks.\nThis landing page is for a fictional finance company written in vanilla HTML, CSS, and JavaScript.\nThe challenge is to make a web page with only the three basic code files: index.html, style.css, and script.js to practice raw web fundamentals. No frameworks, external links, or packages are used. 100% hand-coded. No copypasta or AI."
   },
   {
     name: "Recipe Avalanche",
     github: "https://github.com/EricSorum/recipe-avalanche",
     link: "https://ericsorum.github.io/recipe-avalanche/",
-    description: "hihihi"
+    description: "Looking up recipes doesn't need to take all day.  Most recipe sites require significant loading, ad closing, and scrolling just to view one recipe. Recipe Avalanche reverses the trend by loading up to 16 recipes at a time, all one page, all with one click\nRecipe Avalance is a custom application that uses React.js and the spoonacular.com API to search recipes by ingredients. By pressing one of the ingredient buttons, the user gets a list of recipes using that ingredient.  By extracting only the most essential data from JSON files, Recipe Avalanche provides an image, the ingredients, and instructions for dozens of recipes in an instant.  Features also include a set of buttons to choose how many recipes to list in the search, and a search bar to find recipes for any ingredient.  More features and mobile development are forthcoming."
   },
   {
     name: "Space Ronan",
     github: "https://github.com/EricSorum/space-ronan",
     link: "https://ericsorum.github.io/space-ronan",
-    description: `This simple top-scrolling shooter game shows the potential for AI to improve productivity. Using the Cursor AI integrated development environment (IDE) alongside the Claude Sonnet large language model (LLM), this project took only about three hours.\nWhile Cursor AI allowed for quick development, several roadblocks required me to debug and hand-code several features, such as the collision mechanic and enemy formations.\nOverall, I am excited by the prospect of delivering applications quicker, while appreciating the need to understand each line of code when working with AI.`
-    
+    description: "This simple top-scrolling shooter game shows the potential for AI to improve productivity. Using the Cursor AI integrated development environment (IDE) alongside the Claude Sonnet large language model (LLM), this project took only about three hours.\nWhile Cursor AI allowed for quick development, several roadblocks required me to debug and hand-code several features, such as the collision mechanic and enemy formations.\nOverall, I am excited by the prospect of delivering applications quicker, while appreciating the need to understand each line of code when working with AI.\nInstructions: As a Space Ronan, you are paid a bounty for every invading alien pod you destroy. Use arrows to move and space to fire. Avoid collisions. The difficulty will increase over time."
   }
 ]
 
