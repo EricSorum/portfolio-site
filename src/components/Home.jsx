89d4076415b55f18a5131a55d344d7e8a6e79413
Eric Sorum <ejsorum@gmail.com>
2025-07-17 09:04:16 -0500

    Removed "Other" from skill list.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,16 +17,16 @@ function Home() {
           <li><span className={style.skillCategory}>Frameworks:</span> React.js, Next.js, Symfony</li>
           <li><span className={style.skillCategory}>Style libraries:</span> Bootstrap, Tailwind, Material UI </li>          
           <li><span className={style.skillCategory}>Content Management Systems (CMS):</span> Drupal 8-10, WordPress</li>
-          <li><span className={style.skillCategory}>Dev Tools:</span> Git, GitHub, Vite, Webpack, npm/pnpm, Valet, Homebrew, Composer, Jira</li>
+          <li><span className={style.skillCategory}>Dev Tools:</span> Git, GitHub, npm/pnpm, Homebrew, Composer, Jira, Cursor AI</li>
           <li><span className={style.skillCategory}>Practices:</span> CI/CD, Agile/Scrum development, Accessibility (WCAG 2.2)</li>
-          <li><span className={style.skillCategory}>Other:</span> Cursor AI Agent, LLM-assisted development, Google Tag Manager, Google Analytics 4</li>
+          {/* <li><span className={style.skillCategory}>Other:</span> Cursor AI Agent, LLM-assisted development, Google Tag Manager, Google Analytics 4</li> */}
         </ul>
         <div className={style.resumeButtons}>
           <a href={resume}>
-            <div className={style.resumeButton}>View resume</div>
+            <button className={style.resumeButton}>View resume</button>
           </a>
           <a href={resume} download>
-            <div className={style.resumeButton}>Download resume</div>
+            <button className={style.resumeButton}>Download resume</button>
           </a>
         </div>
       </div>
